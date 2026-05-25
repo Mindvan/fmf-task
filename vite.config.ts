7f0fd7abd50a40e5daf68d042ca45e81124d65b3
Ivan Eroshin <itseroshin@gmail.com>
2026-05-25 12:11:02 +0300

    feat: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/fmf-task/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
